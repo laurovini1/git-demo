92c219e6ebd0007297c869c67d4f5e95dbaa11e4
Lauro Oliveira <lauro.vinicius.oliveira@gmail.com>
2022-08-16 16:55:53 -0300

    Curso

---
+++ demo/curso-rocketseat/Fetch-Discovery-API/main.js
@@ -0,0 +1,32 @@
+const url = "http://localhost:5500/api"
+
+function getUsers(){
+    fetch(url)
+        .then(response => response.json())
+        .then(data => renderApiResult.textContent = JSON.stringify(data))
+        .catch(error => console.error(error))
+}
+
+function getUser(){
+    fetch(`${url}/1`)
+    .then(response => response.json())
+    .then(data => {
+        userName.textContent = data.name
+        userCity.textContent = data.city
+        userAvatar.src = data.avatar
+    })
+    .catch(error => console.log(error))
+}
+
+function addUser(){
+    
+}
+
+const newUser = {
+    name: "Olivia Zars",
+    avatar: "http://lorempixel.com/400/200",
+    city: "Rio do Sul"
+}
+
+getUser()
+getUsers()
