a80ca7451411f2d1f6ef3b30da09e98ae9d9ee64
Lauro Oliveira <lauro.vinicius.oliveira@gmail.com>
2022-08-18 08:49:41 -0300

    Fetch-Discovery

--- demo/curso-rocketseat/Fetch-Discovery-API/main.js
+++ demo/curso-rocketseat/Fetch-Discovery-API/main.js
@@ -18,15 +18,45 @@ function getUser(){
     .catch(error => console.log(error))
 }
 
-function addUser(){
-    
+function addUser(newUser){
+    fetch(url, {
+        method: "POST",
+        body: JSON.stringify(newUser),
+        headers: {
+            "Content-type": "application/json;charset=UTF-8"
+        }
+    })
+        .then(response => response.json())
+        .then(data => alertApi.textContent = data)
+        .catch(error => console.log(error))
+}
+
+function updateUser(updatedUser) {
+    fetch(`${url}/1`, {
+        method: "PUT",
+        body: JSON.stringify(updatedUser),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    })
+        .then(response => response.json())
+        .then(data => alertApi.textContent = data)
+        .catch(error => console.log(error))
 }
 
 const newUser = {
-    name: "Olivia Zars",
-    avatar: "http://lorempixel.com/400/200",
+    name: "Ricardo Goulart",
+    avatar: "https://picsum.photos/200/300",
     city: "Rio do Sul"
 }
 
+const updatedUser = {
+    name: "Everton Ribeiro",
+    avatar: "https://picsum.photos/200/300",
+    city: "Belo Horizonte"
+}
+
+updateUser(updateUser)
 getUser()
 getUsers()
+//addUser(newUser)
