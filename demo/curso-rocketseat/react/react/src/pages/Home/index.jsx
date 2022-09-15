e11e370b9a587f3ac246e2f89b87e7855880c6c1
Lauro Oliveira <lauro.vinicius.oliveira@gmail.com>
2022-09-15 15:33:17 -0300

    React

--- demo/curso-rocketseat/react/react/src/pages/Home/index.jsx
+++ demo/curso-rocketseat/react/react/src/pages/Home/index.jsx
@@ -1,13 +1,70 @@
+import React, { useState, useEffect } from 'react';
 import './style.css';
+import {Card} from '../../components/Card';
 
 
 export function Home() {
+  const[studentName, setStudentName] = useState('Nome');
+  const[students, setStudents] = useState([]);
+  const[user, setUser] = useState({name: '', avatar:''});
+
+  function hadleAddStudent(){
+    const newStudent = {
+      name: studentName,
+      time: new Date().toLocaleDateString("pt-br", {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+      })
+    };
+
+    setStudents(prevState => [...prevState, newStudent]);
+
+  }
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/laurovini1')
+    .then(response => response.json())
+    .then(data => {
+      setUser({
+        name: data.name,
+        avatar: data.avatar_url,
+      })
+
+    })
+  },[]);
 
   return (
     <div className="container">
-      <h1>Lista de Presença</h1>
-      <input type="text" placeholder="Digite o nome" />
-      <button type="button">Adicionar</button>
+
+      <header>
+        <h1>Lista de Presença</h1>
+        <div>
+          <strong>{user.name}</strong>
+          <img src={user.avatar} alt="Foto de perfil" />
+        </div>
+      </header>
+
+      <input
+      type="text"
+      placeholder="Digite o nome"
+      onChange={e => setStudentName(e.target.value)}
+      />
+
+      <button type="button" onClick={hadleAddStudent}>
+        Adicionar
+      </button>
+
+      {
+        students.map(student => (
+        <Card 
+          key={student.time}
+          name={student.name} 
+          time={student.time}
+          />
+        ))
+      
+      }
     </div>    
   )
 }
